d70cd5e1c5abff17290fa018b8be028fd77a0722
Zhlobo Dmitry <dmitry.zhlobo@yandex.ru>
2022-09-24 17:48:45 +0300

    Add correct word to age

--- main.py
+++ main.py
@@ -6,17 +6,30 @@ import datetime
 
 year_of_start = 1920
 now = datetime.datetime.now().strftime('%Y')
-age_of_winery = int(now) - 1920
+age_of_winery = int(now) - 2015
 
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
+
+def correct_years(year):
+    goda = [2, 3, 4]
+    negoda = [12, 13, 14]
+    if (year % 10 == 1) and (year % 100 != 11):
+        return "год"
+    elif (year % 10 in goda) and (year % 100 not in negoda):
+        return "года"
+    else:
+        return 'лет'
+
+
 template = env.get_template('template.html')
 
 rendered_page = template.render(
     age=age_of_winery,
+    correct_word=correct_years(age_of_winery)
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
