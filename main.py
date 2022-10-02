8811ddea70510d73a4ddb36a84d84dea8c80a7f0
Dim4ik8 <87538630+Dim4ik8@users.noreply.github.com>
2022-10-02 15:59:32 +0300

    Add argparse, update README.md

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
 import collections
+import argparse
 
-year_of_start = 1920
-now = datetime.datetime.now().strftime('%Y')
-age_of_winery = int(now) - year_of_start
+foundation_year = 1920
+now = datetime.datetime.now().year
+winery_age = int(now) - foundation_year
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -17,35 +18,45 @@ env = Environment(
 
 
 def correct_years(year):
-    goda = [2, 3, 4]
-    negoda = [12, 13, 14]
+    for_year = [2, 3, 4]
+    for_years = [12, 13, 14]
     if (year % 10 == 1) and (year % 100 != 11):
         return "год"
-    elif (year % 10 in goda) and (year % 100 not in negoda):
+    elif (year % 10 in for_year) and (year % 100 not in for_years):
         return "года"
     else:
         return 'лет'
 
 
-excel_data = pandas.read_excel('wine3.xlsx', sheet_name='Лист1',
-                               usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'], na_values=None,
-                               keep_default_na=False)
-all_wine = excel_data.to_dict(orient='records')
-all_wine_by_categories = collections.defaultdict(list)
+def main():
+    parser = argparse.ArgumentParser(
+        description='Сайт производителей вина и других напитков'
+    )
+    parser.add_argument('-p', '--path', help='Путь в файлу с данными', default='wine3.xlsx')
+    parser.add_argument('-s', '--sheet', help='Название рабочего листа', default='Лист1')
+    args = parser.parse_args()
 
-for item in all_wine:
-    all_wine_by_categories[item['Категория']].append(item)
+    excel_data = pandas.read_excel(args.path, sheet_name=args.sheet, na_values=None, keep_default_na=False)
+    all_wine = excel_data.to_dict(orient='records')
+    all_wine_by_categories = collections.defaultdict(list)
 
-template = env.get_template('template.html')
+    for category in all_wine:
+        all_wine_by_categories[category['Категория']].append(category)
 
-rendered_page = template.render(
-    age=age_of_winery,
-    correct_word=correct_years(age_of_winery),
-    wine=all_wine_by_categories,
-)
+    template = env.get_template('template.html')
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    rendered_page = template.render(
+        age=winery_age,
+        correct_word=correct_years(winery_age),
+        wine=all_wine_by_categories,
+    )
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
